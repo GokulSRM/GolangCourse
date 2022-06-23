72390223fa5f280698ac57b9f81b446034346349
GokulSRM <gokulramaswamy@srmtech.com>
2022-06-24 00:10:51 +0530

    day 1

--- Card/main.go
+++ Card/main.go
@@ -4,4 +4,12 @@ import "fmt"
 
 func main() {
 	fmt.Println("Five of Spades")
+	// var cards string = "Five of Spades"
+	cards := "Five of Spades"
+	fmt.Println(cards)
+	res := 0
+	for i := 0; i < 10; i += 3 {
+		res += i
+	}
+	fmt.Println(res)
 }
