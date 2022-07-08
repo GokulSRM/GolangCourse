71a2c64113e05c904b7b43f38118bfc2deb0cd4b
GokulSRM <gokulramaswamy@srmtech.com>
2022-07-08 21:04:57 +0530

    update

--- Card/main.go
+++ Card/main.go
@@ -6,7 +6,7 @@ func main() {
 	fmt.Println("Five of Spades")
 	// var cards string = "Five of Spades"
 	// cards := "Five of Spades"
-
+	
 	cards := deck{"Five of hearts", "Three Diamonds", newCard()}   //type deck is of []string from deck.go
 	cards = append(cards, "Six of Hearts")
 	// fmt.Println(cards)
