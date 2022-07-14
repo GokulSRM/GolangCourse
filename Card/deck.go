0ad60c304727705e5656efc330104b44f6cb9d88
GokulSRM <gokulramaswamy@srmtech.com>
2022-07-14 15:13:39 +0530

    File read and write update

--- Card/deck.go
+++ Card/deck.go
@@ -1,7 +1,10 @@
 package main
 
-import ("fmt"
-"strings"
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
 )
 
 type deck []string
@@ -35,4 +38,21 @@ func (d deck) toString() string{
 	return strings.Join([]string(d),",")
 }
 
+func (d deck) saveToFile (filename string) error{
+	 return ioutil.WriteFile(filename, []byte(d.toString()),0666) //0666 means any one can ready and write a file
+}
+
+func newDeckFromFile (filename string) deck {
+	bs, err := ioutil.ReadFile(filename)
+
+	if err != nil{
+		    //Option #1 - Log the error and return a call to newDeck()
+			// Option #2 -  Log the error and entirely quit the program
+			 fmt.Println("Error: ", err)
+			 os.Exit(1)
+	}
+	s:= strings.Split(string(bs),",")
+	return  deck(s)
+
+}
  
--- Card/main.go
+++ Card/main.go
@@ -3,14 +3,15 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Five of Spades")
+	fmt.Println("-----------Cards Project----------")
+	fmt.Println("----------------------------------")
 	// var cards string = "Five of Spades"
 	// cards := "Five of Spades"
 
 	// cards := deck{"Five of hearts", "Three Diamonds", newCard()}   //type deck is of []string from deck.go
 	// cards = append(cards, "Six of Hearts")
 	// fmt.Println(cards)
-	cards := newDeck()
+	// cards := newDeck()
 
 	// hand, remainingCards := deal(cards, 5)
 	// // cards.print()
@@ -21,10 +22,17 @@ func main() {
 	// greeting := "Hi There"
 	// fmt.Println([]byte(greeting))
 
-	fmt.Println(cards.toString())
+	// fmt.Println(cards.toString())
+
+	// fmt.Println(cards.saveToFile("my_cards"))
+
 	// for i, card := range cards {
 	// 	fmt.Println(i, card)
 	// }
+
+	cards := newDeckFromFile("my")
+
+	cards.print()
 }
 
 // func newCard() string {
